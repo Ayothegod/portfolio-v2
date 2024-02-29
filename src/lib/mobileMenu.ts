7a98f07e6ca712e0f66e9351182dbb0818874b5e
Ayo <heyayomideadebisi@gmail.com>
2024-02-29 21:09:48 +0100

    feat: implement active icon state based on url.pathname

--- src/lib/mobileMenu.ts
+++ src/lib/mobileMenu.ts
@@ -1,32 +1,38 @@
 interface Icon {
   href: string;
-  icon: string;
+  icon: string
+  activeIcon?: string;
 }
 
 export const mobileMenuIcons: Icon[] = [
   {
     href: "/",
-    icon: "icons/Home - Fill",
+    icon: "icons/Home - Outline",
+    activeIcon: "icons/Home - Fill",
   },
   {
     href: "/about",
     icon: "icons/User - Outline",
+    activeIcon: "icons/User - Fill",
   },
   {
     href: "/work",
     icon: "icons/Bag - Outline",
+    activeIcon: "icons/Bag - Fill",
   },
   {
     href: "/notebook",
     icon: "icons/Pen - Outline",
+    activeIcon: "icons/Pen - Fill",
   },
   {
     href: "/contact",
     icon: "icons/Message - Outline",
+    activeIcon: "icons/Message - Fill",
   },
   {
     href: "/more",
     icon: "icons/More - Outline",
+    activeIcon: "icons/More - Fill",
   },
-
 ];
