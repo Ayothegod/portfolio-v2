12e3971cc0e98cb481257ec7b1ce4c34f1ff2500
Ayo <heyayomideadebisi@gmail.com>
2024-02-29 14:21:37 +0100

    feat: implement basse mobile menu

---
+++ src/lib/mobileMenu.ts
@@ -0,0 +1,32 @@
+interface Icon {
+  href: string;
+  icon: string;
+}
+
+export const mobileMenuIcons: Icon[] = [
+  {
+    href: "/",
+    icon: "icons/Home - Fill",
+  },
+  {
+    href: "/about",
+    icon: "icons/User - Outline",
+  },
+  {
+    href: "/work",
+    icon: "icons/Bag - Outline",
+  },
+  {
+    href: "/notebook",
+    icon: "icons/Pen - Outline",
+  },
+  {
+    href: "/contact",
+    icon: "icons/Message - Outline",
+  },
+  {
+    href: "/more",
+    icon: "icons/More - Outline",
+  },
+
+];
