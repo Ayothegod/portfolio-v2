e348a5ab1544057ff271a8ad829f6a635508b998
Ayo <heyayomideadebisi@gmail.com>
2024-03-04 20:28:10 +0100

    build: shadcn write to my directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
   prefix: "",
   theme: {
     container: {
@@ -12,16 +17,19 @@ module.exports = {
       },
     },
     extend: {
-      fontFamily:{
-        "inter": ['Inter', 'sans-serif'],
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
-      colors: {
-        dark:"#0D0D0D",
-        light:"#fbfbfb",
-        "dark-secondary":"#181818",
-        "light-secondary":"#F6F6F6",
-        "dark-hover":"#242424",
-        "light-hover":"#E5E5E5",
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
