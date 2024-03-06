d0a8f097674b87444d6902c2b7b36339d2171cac
Ayo <heyayomideadebisi@gmail.com>
2024-03-06 20:49:14 +0100

    feat: add image to featured works on desktop

--- src/lib/mobileMenu.ts
+++ src/lib/mobileMenu.ts
@@ -14,6 +14,16 @@ interface Nav {
   name: string;
 }
 
+interface Project {
+  id: number;
+  icon: string;
+  image: string;
+  title: string;
+  description: string;
+  link: string;
+  github: string;
+}
+
 export const mobileMenuIcons: Icon[] = [
   {
     href: "/",
@@ -84,32 +94,35 @@ export const desktopIcon: DesktopIcon[] = [
   },
 ];
 
-export const mockProjects = [
+export const mockProjects: Project[] = [
   {
-    id:1,
+    id: 1,
     icon: "icons/Behance",
-    image: "/assets/tech/subbi.png",
-    title:"Subbi –– The free subscriptions manager",
-    description:"Subbi is a side project that I’ve built to help me keep track of how much I spend on subscriptions and also to prevent the “accidental” bill after a 14-day trail ends. It helps you keep track of bills like Netflix, Spotify, Xbox Game Pass, Bus Card, Bank Cards, and many more.",
-    link:"http://ayomide.me",
-    github:"https://github.com/ayothegod"
+    image: "src/assets/tech/subbi.png",
+    title: "Subbi –– The free subscriptions manager",
+    description:
+      "Subbi is a side project that I’ve built to help me keep track of how much I spend on subscriptions and also to prevent the “accidental” bill after a 14-day trail ends. It helps you keep track of bills like Netflix, Spotify, Xbox Game Pass, Bus Card, Bank Cards, and many more.",
+    link: "http://ayomide.me",
+    github: "https://github.com/ayothegod",
   },
   {
-    id:2,
+    id: 2,
     icon: "icons/Discord",
-    image: "/assets/tech/Discord.png",
-    title:"AsciiFolders",
-    description:"Generate ASCII directory trees from any folder on your machine, customize the way the tree looks, how deep it should go and whether or not to include hidden files",
-    link:"http://ayomide.me",
-    github:"https://github.com/ayothegod"
+    image: "src/assets/tech/Parrotxt.png",
+    title: "AsciiFolders",
+    description:
+      "Generate ASCII directory trees from any folder on your machine, customize the way the tree looks, how deep it should go and whether or not to include hidden files",
+    link: "http://ayomide.me",
+    github: "https://github.com/ayothegod",
   },
   {
-    id:3,
+    id: 3,
     icon: "icons/Dribbble",
-    image: "/assets/tech/React Docs.png",
-    title:"React Documentation",
-    description:"With the release of the new React website on March 16, there was a need for localization to make the documentation accessible to Arabic-speaking developers. I contributed to translating several documentation, worked with great developers, Improved my ability to write better PR requests, and as a side effect, I now know more about the Arabic grammar.",
-    link:"http://ayomide.me",
-    github:"https://github.com/ayothegod"
+    image: "src/assets/tech/React Docs.png",
+    title: "React Documentation",
+    description:
+      "With the release of the new React website on March 16, there was a need for localization to make the documentation accessible to Arabic-speaking developers. I contributed to translating several documentation, worked with great developers, Improved my ability to write better PR requests, and as a side effect, I now know more about the Arabic grammar.",
+    link: "http://ayomide.me",
+    github: "https://github.com/ayothegod",
   },
-]
+];
