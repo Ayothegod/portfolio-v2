faba5a08bfbce196fb75750e515b2bcab57c88b4
Ayo <heyayomideadebisi@gmail.com>
2024-03-06 20:17:45 +0100

    feat: build featured products ui

--- src/lib/mobileMenu.ts
+++ src/lib/mobileMenu.ts
@@ -86,11 +86,30 @@ export const desktopIcon: DesktopIcon[] = [
 
 export const mockProjects = [
   {
+    id:1,
     icon: "icons/Behance",
-    image: "/assets/",
+    image: "/assets/tech/subbi.png",
     title:"Subbi –– The free subscriptions manager",
     description:"Subbi is a side project that I’ve built to help me keep track of how much I spend on subscriptions and also to prevent the “accidental” bill after a 14-day trail ends. It helps you keep track of bills like Netflix, Spotify, Xbox Game Pass, Bus Card, Bank Cards, and many more.",
     link:"http://ayomide.me",
-    github:"http://ayomide.me"
-  }
+    github:"https://github.com/ayothegod"
+  },
+  {
+    id:2,
+    icon: "icons/Discord",
+    image: "/assets/tech/Discord.png",
+    title:"AsciiFolders",
+    description:"Generate ASCII directory trees from any folder on your machine, customize the way the tree looks, how deep it should go and whether or not to include hidden files",
+    link:"http://ayomide.me",
+    github:"https://github.com/ayothegod"
+  },
+  {
+    id:3,
+    icon: "icons/Dribbble",
+    image: "/assets/tech/React Docs.png",
+    title:"React Documentation",
+    description:"With the release of the new React website on March 16, there was a need for localization to make the documentation accessible to Arabic-speaking developers. I contributed to translating several documentation, worked with great developers, Improved my ability to write better PR requests, and as a side effect, I now know more about the Arabic grammar.",
+    link:"http://ayomide.me",
+    github:"https://github.com/ayothegod"
+  },
 ]
