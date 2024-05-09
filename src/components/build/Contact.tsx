be9e261202362de52b1be157739ca2c051f397a5
Ayo <heyayomideadebisi@gmail.com>
2024-05-09 23:23:58 +0100

    task: add shacn iput

--- src/components/build/Contact.tsx
+++ src/components/build/Contact.tsx
@@ -1,7 +1,7 @@
 export default function ContactAction() {
   return (
-    <div className="bg-light-secondary dark:bg-dark-secondary border border-light-hover dark:border-dark-hover rounded-md">
-      <h3 className="text-center text-dark dark:text-light block font-bold border-b p-4">
+    <div className="box">
+      <h3 className="text text-center block font-bold border-b p-4">
         New Message
       </h3>
 
@@ -9,7 +9,7 @@ export default function ContactAction() {
         <div className="flex">
             <label htmlFor="email" className="hidden md:block">E-mail</label>
             {/* <input type="text" name="email" placeholder="Enter your email" className="w-full"/> */}
-            
+
         </div>
       </div>
     </div>
